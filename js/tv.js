3e1614dd361a42dd4054bfb74e80510a0026d9aa
Dean Putney <putney.dean@gmail.com>
2013-11-14 15:31:59 -0800

    Hotkeys working with object

--- js/tv.js
+++ js/tv.js
@@ -192,27 +192,27 @@ var RedditTV = Class.extend({
 				var keyCode = e.keyCode || e.which, arrow = {left: 37, up: 38, right: 39, down: 40 };
 				switch (keyCode) {
 				case arrow.left:  case 72: // h
-					loadVideo('prev');
+					rtv.loadVideo('prev');
 					break;
 				case arrow.up:	case 75: // k
-					chgChan('up');
+					rtv.chgChan('up');
 					break;
 				case arrow.right: case 76: // l
-					loadVideo('next');
+					rtv.loadVideo('next');
 					break;
 				case arrow.down:  case 74: // j
-					chgChan('down');
+					rtv.chgChan('down');
 					break;
 				case 32:
-					togglePlay();
+					rtv.togglePlay();
 					break;
 				case 70:
 					$('#fill').attr('checked', true);
-					fillScreen();
+					rtv.fillScreen();
 					break;
 				case 27:
 					if($('#fill').is(':checked')){
-						fillScreen();
+						rtv.fillScreen();
 					}
 					break;
 				case 67:
